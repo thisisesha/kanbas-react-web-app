2d610ff45bd18df25fb5738b209d59529844f0b6
Esha Chiplunkar <eshac7@gmail.com>
2024-04-03 11:11:26 -0400

    lab till signup complete

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,6 +1,7 @@
 import Profile from "../../Users/Profile";
 import Signin from "../../Users/Signin";
 import { Routes, Route, Navigate } from "react-router-dom";
+import UserTable from "../../Users/Table";
 export default function Account() {
   return (
     <div className="container-fluid">
@@ -8,6 +9,7 @@ export default function Account() {
         <Route path="/" element={<Navigate to="/Kanbas/Account/Signin" />} />
         <Route path="/Signin" element={<Signin />} />
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/Admin/Users" element={<UserTable />} />
       </Routes>
     </div>
   );
