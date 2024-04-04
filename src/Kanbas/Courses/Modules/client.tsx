b6f0c8cf6c726b4df7d501f628da843eaefe1b71
Esha Chiplunkar <eshac7@gmail.com>
2024-04-03 23:00:37 -0400

    a5 api changes

--- src/Kanbas/Courses/Modules/client.tsx
+++ src/Kanbas/Courses/Modules/client.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
-const COURSES_API = `${API_BASE}/api/courses`;
-const MODULES_API = `${API_BASE}/api/modules`;
+const COURSES_API = `${API_BASE}api/courses`;
+const MODULES_API = `${API_BASE}api/modules`;
 
 export const updateModule = async (module: { _id: any; }) => {
     const response = await axios.
